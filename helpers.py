cc2f2db2073ace6c4eee61f708095a1f3db10b0c
Brandon Johnson <bbj1979@gmail.com>
2020-08-04 20:28:25 -0600

    Moved methods out of file_system into helpers

--- helpers.py
+++ helpers.py
@@ -1,10 +1,14 @@
+import errno
 import os
+from pathlib import Path
 from typing import List
+
+from illegal_file_system_operation import IllegalFileSystemOperation
 
 MAIN_DRIVE = '/root'
 
 
-def file_path(parent_path: str, name: str):
+def file_path(parent_path: str, name: str) -> str:
     """
     Get a file path given the parent path and name of the file system entity
     :param parent_path: str - The full string path of the parent
@@ -14,7 +18,7 @@ def file_path(parent_path: str, name: str):
     return os.path.join(parent_path, name)
 
 
-def recursive_size_update(node):
+def recursive_size_update(node) -> None:
     """
     Recursively update the size attribute upward through the file system tree
     :param node: FileSystemEntity
@@ -33,4 +37,32 @@ def calc_size(children: List, size: int = 0):
     """
     for child in children.values():
         size += child.size
+
     return size
+
+
+def get_parent_path_and_name(full_path: str):
+    if not full_path:
+        return None, None
+
+    path = Path(full_path)
+    parent_path = path.parent
+    name = path.parts[-1]
+
+    return parent_path, name
+
+
+def valid_move_operation(dest_path: str, dest_name: str, dest_parent_node, source_path: str, source_node) -> bool:
+    # Raise if attempting to move entity into itself
+    if dest_parent_node == source_node:
+        raise IllegalFileSystemOperation(errno.ENOENT, os.strerror(errno.ENOENT), dest_parent_node)
+    # Raise if the source node or parent path doesn't exist
+    if not source_node or not dest_parent_node:
+        missing_path = source_path if not source_node else dest_path
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), missing_path)
+    # Raise if destination path already exists
+    if hasattr(dest_parent_node, 'children') and dest_parent_node.children.get(dest_name):
+        raise FileExistsError(errno.EEXIST, os.strerror(errno.EEXIST), dest_path)
+
+    return True
+
